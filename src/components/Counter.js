17767f285b989a410b4ade22fdad801bdae0e6b9
Deadpool1 <atwood169@yahoo.com>
2023-07-13 11:59:58 -0500

    state and css

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,13 +4,13 @@ class Counter extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            counter: 0
         }
     }
     render() {
         return (
-            <div>
-
+            <div className="counter">
+                Counter: {this.state.counter}
             </div>
         )
     }
